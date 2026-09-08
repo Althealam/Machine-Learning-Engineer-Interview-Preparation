769f578ed79d2daf264e3a3152f4e70d8c0c93f7
Althea Lam <althealam0531@gmail.com>
2026-09-08 13:22:36 +0800

    auc

--- Part 6: interview prepare/Quora/Algo Interview/Find Numbers with Even Number of Digits.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findNumbers(self, nums: List[int]) -> int:
-        cnt = 0
-        for num in nums:
-            if len(str(num))%2==0:
-                cnt+=1
-        return cnt
