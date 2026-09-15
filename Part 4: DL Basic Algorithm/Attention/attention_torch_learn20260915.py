53d5490e53b00f8e03a57ec653285a119aad2808
Althea Lam <althealam0531@gmail.com>
2026-09-15 22:12:36 +0800

    attention代码

--- Part 4: DL Basic Algorithm/Attention/attention_torch_learn20260915.py
+++ Part 4: DL Basic Algorithm/Attention/attention_torch_learn20260915.py
@@ -3,33 +3,33 @@ import torch
 
 def attention(query, key, value):
     """
-    input: 
-        query: (b, query_len, d_k)
-        key: (b, key_len, d_k)
-        value: (b, key_len, d_v)
-
-    output:
-        outputs: (b, query_len, d_v)
-        weights: (b, query_len, key_len)
+    input:
+        - query: [b, query_len, d_k]
+        - key: [b, key_len, d_k]
+        - value: [b, key_len, d_v]
+    output: 
+        - outputs: [b, query_len, d_v]
+        - weight: [b, query_len, key_len]
     """
 
     d_k = query.size(-1)
 
-    # QK^T/sqrt(d_k)
+    # Q: [b, query_len, d_k]
+    # K^T: [b, d_k, key_len]
+    # QK^T: [b, query_len, key_len]
     scores = torch.matmul(query, key.transpose(-2, -1))
     scores = scores/math.sqrt(d_k)
 
+    # weights: [b, query_len, key_len]
     weights = torch.softmax(scores, dim=-1)
-
+    # output: [b, query_len, d_v]
     output = torch.matmul(weights, value)
-
     return output, weights
 
 batch_size = 2
 seq_len = 4
 d_k = 8
 d_v = 6
-
 query = torch.randn(batch_size, seq_len, d_k)
 key = torch.randn(batch_size, seq_len, d_k)
 value = torch.randn(batch_size, seq_len, d_v)
